7da0121493c9988715ff49d8a94ba7543379f551
Mehmet Afif ARSAY <52586509+mehmetarsay@users.noreply.github.com>
2021-06-09 17:14:48 +0300

    showModalBottomSheet added

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -32,20 +32,21 @@ class _HomePageState extends BaseState<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context),
-      body:  context.watch<HomePageViewMopdel>().wait==false?SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            for (var i in context.read<HomePageViewMopdel>().dataList)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: sharingView(i),
-              ),
-          ],
-        ),
-      ):Center(child: CircularProgressIndicator())
-    );
+        appBar: buildAppBar(context),
+        body: context.watch<HomePageViewMopdel>().wait == false
+            ? SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    for (var i in context.read<HomePageViewMopdel>().dataList)
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: sharingView(i),
+                      ),
+                  ],
+                ),
+              )
+            : Center(child: CircularProgressIndicator()));
   }
 
   AppBar buildAppBar(BuildContext context) {
@@ -72,34 +73,43 @@ class _HomePageState extends BaseState<HomePage> {
           ),
         ],
       ),
-      actions: [
-        themeChangeWidget(context)
-      ],
+      actions: [themeChangeWidget(context)],
     );
   }
 
   GestureDetector themeChangeWidget(BuildContext context) {
     return GestureDetector(
-          onTap: (){
-            Provider.of<ThemeNotifier>(context, listen: false).changeValue();
-          },
-          //currentTheme == AppThemeLight.instance?.theme
-          child: Row(
-            children: [
-              Icon(
-                  Provider.of<ThemeNotifier>(context, listen: false).currentTheme != AppThemeLight.instance?.theme?Icons.light_mode:Icons.dark_mode,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              SizedBox(width: 10,)
-
-            ],
+      onTap: () {
+        Provider.of<ThemeNotifier>(context, listen: false).changeValue();
+      },
+      //currentTheme == AppThemeLight.instance?.theme
+      child: Row(
+        children: [
+          Icon(
+            Provider.of<ThemeNotifier>(context, listen: false).currentTheme !=
+                    AppThemeLight.instance?.theme
+                ? Icons.light_mode
+                : Icons.dark_mode,
+            color: Theme.of(context).colorScheme.primary,
           ),
-        );
+          SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+    );
   }
 
   Widget sharingView(ShareModel share) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        showModalBottomSheet<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return showBottomSheet(share, context);
+          },
+        );
+      },
       child: Container(
         height: dynamicHeight(0.2),
         width: dynamicWidth(1),
@@ -132,9 +142,7 @@ class _HomePageState extends BaseState<HomePage> {
                     child: Text(
                       'Score:${share.score}',
                       style: TextStyle(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .primary,
+                          color: Theme.of(context).colorScheme.primary,
                           fontWeight: FontWeight.w500,
                           fontSize: 12),
                     ),
@@ -195,6 +203,73 @@ class _HomePageState extends BaseState<HomePage> {
               ],
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Padding showBottomSheet(ShareModel share, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 15),
+      child: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      '${share.author}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.7),
+                          fontSize: 12),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      'Score:${share.score}',
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                width: dynamicWidth(0.9),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    '${share.title}',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+              if (share.thumbnail != 'self')
+                Container(
+                  child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Image.network(
+                        '${share.thumbnail}',
+                        fit: BoxFit.fill,
+                      )),
+                ),
+              Container(
+                child: Text('${share.selftext}'),
+              ),
+            ],
+          ),
         ),
       ),
     );
